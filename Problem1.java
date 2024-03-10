42afca0022d8f2b377dccb7417e28c23cc1694c0
Kyle Trom <ktrom@iastate.edu>
2024-03-09 17:55:12 -0800

    first two euler problems

---
+++ Problem1.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Problem1{
+    public static void main(String[] args) {
+        // Checking every value if divisible by 3 or 5
+        // Optimize if
+        System.out.println(IntStream.range(0, 1000).filter(val -> val % 3 == 0 || val % 5 == 0).sum());
+    }
+}
